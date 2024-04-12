63e34a5a5a254980ec33b00dbf1822c20064fb04
Enrique00007-uam <emsolis@uamv.edu.ni>
2024-04-12 07:43:56 -0600

    first commit

---
+++ arregloFlotante.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//almazenar 6 nuemros flotantes
+
+int main(int argc, char const *argv[])
+{
+    float numbers[6];
+    numbers[0]=12;
+    numbers[1]=1.68;
+    numbers[2]=2.698;
+    numbers[3]=3.5;
+    numbers[4]=1512.9999;
+    numbers[5]=3.462873;
+    
+    printf("elemento #1 %f\n", numbers[0]);
+    printf("elemento #2 %f\n", numbers[1]);
+    printf("elemento #3 %f\n", numbers[2]);
+    printf("elemento #4 %f\n", numbers[3]);
+    printf("elemento #5 %f\n", numbers[4]);
+    printf("elemento #6 %f\n", numbers[5]);
+    
+    
+    return 0;
+}
+    
