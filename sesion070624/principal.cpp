062ce1d9bf1bdbf9b99dd73d805ceb9ff96f90ff
Enrique00007-uam <emsolis@uamv.edu.ni>
2024-06-07 10:45:31 -0600

    agregue sesion 070624, lo agregue despues de la clase pq se subio mal el anterior archivo

---
+++ sesion070624/principal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include "funciones.cpp"
+#include <locale.h>
+
+int main(int argc, char const *argv[])
+{
+    setlocale(LC_ALL, "spanish");
+   principal();
+    return 0;
+}
